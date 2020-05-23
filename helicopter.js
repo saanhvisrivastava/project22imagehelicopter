8306f7bf796cd9fe404f43599ba3f7cf0f3c7262
saanhvisrivastava <63591636+saanhvisrivastava@users.noreply.github.com>
2020-05-23 16:16:25 +0530

    Add files via upload

---
+++ helicopter.js
@@ -0,0 +1,8 @@
+class Helicopter extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/helicopter.jpg");
+    }
+  
+  };
+  
